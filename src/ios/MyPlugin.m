844812094529ae7d7c6d2c3d0fc2a22aa7577554
codigoabierto <jorgeteus@gmail.com>
2013-09-20 12:40:35 -0500

    Fix for messagesAsString for messageAsString

--- src/ios/MyPlugin.m
+++ src/ios/MyPlugin.m
@@ -11,7 +11,7 @@
 	if(echo != nil && [echo length] > 0){
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
 	} else {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messagesAsString:@"Arg was null"];
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
 	}
 
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
