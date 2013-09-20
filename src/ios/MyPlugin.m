118d9b16b376bf0612397efd535cde436099f7a9
codigoabierto <jorgeteus@gmail.com>
2013-09-20 12:02:30 -0500

    Adding all the project base files

---
+++ src/ios/MyPlugin.m
@@ -0,0 +1,21 @@
+#import "MyPlugin.h"
+#import <Cordova/CDV.h>
+
+@implementation MyPlugin
+
+- (void) echo:(CDVInvokedURLCommand*)command{
+
+	CDVPluginResult* pluginResult = nil;
+	NSString* echo = [command.arguments objectAtIndex:0];
+
+	if(echo != nil && [echo length] > 0){
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+	} else {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messagesAsString:@"Arg was null"];
+	}
+
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+@end
